d57189607aec57a549c86c0059412e7906aebfd5
phm-oh <oh@udvc.ac.th>
2023-11-08 22:56:24 +0700

    'bot'

--- main.py
+++ main.py
@@ -1,54 +1,10 @@
+from windowCapture import *
+import cv2  as cv
 
-import cv2 as cv 
-import numpy as np
-# print(cv)
-# print(np)
-
-# read img
-main_img = cv.imread('image/peple.jpg',cv.IMREAD_ANYCOLOR)
-temp_img = cv.imread('image/temp.jpg',cv.IMREAD_ANYCOLOR)
-# print(temp_img)
-# print(main_img)
-# print(type(temp_img))
-# print(type(main_img))
-
-
-result = cv.matchTemplate(main_img,temp_img,cv.TM_CCOEFF_NORMED)
-# print(result)
-
-# min,max,minloc,maxloc = cv.minMaxLoc(result)
-_,maxvalue,_,maxloc = cv.minMaxLoc(result)
-
-print(maxvalue)
-
-print(maxloc)
-threshold = 0.9
-
-if maxvalue >= threshold:
-    topleft = maxloc
-    print(temp_img.shape)
-    
-    #ระบุความกว้างความสูง
-    height = temp_img.shape[0]
-    width = temp_img.shape[1]
-     
-    #กำหนดตำแหน่งที่จะวาดรูปสี่เหลี่ยม
-    buttomright = (topleft[0]+width,topleft[1]+height)
-
-     #วาดรูปสี่เหลี่ยม 
-    cv.rectangle(main_img,topleft,buttomright,color=(97,189,92) , thickness=4, lineType= cv.LINE_4)
-
-    
-    #ใส่ตัวหนังสือ
-    font = cv.FONT_ITALIC
-    #ตำแหน่ง
-    position = (topleft[0]+30,topleft[1]-5)
-    fontsize = 0.5
-    color = (255,0,255)
-    cv.putText(main_img,"TEST",position,font,fontsize,color,thickness=2)
-
-    cv.imshow('result',main_img)
-    cv.waitKey()
-    cv.destroyAllWindows()
-
-
+windows = WindowCapture('Media Player')
+# print(windows)
+screen = windows.screenshot()
+# print(screen)
+# cv.imshow('test',screen)
+# cv.waitKey()
+# cv.destroyAllWindows()
---
+++ sourceCodeClassbot/Classbot4_debug_param.py
@@ -0,0 +1,65 @@
+import cv2 as cv
+import numpy as np
+
+class Classbot :
+    def __init__(self,main_img,temp_img):
+        self.mainimg = cv.imread(main_img,cv.IMREAD_ANYCOLOR)
+        self.temp_img = cv.imread(temp_img,cv.IMREAD_ANYCOLOR)
+        
+
+    def search(self,acc=0.9,debug=False)  :
+        result =   cv.matchTemplate(self.mainimg,self.temp_img,cv.TM_CCOEFF_NORMED)
+
+        _,maxvalue,_,maxloc = cv.minMaxLoc(result)
+
+        # acc = 0.9
+            
+        locations = np.where(result >= acc)  
+        locations = list(zip(*locations[::-1])) 
+
+        
+
+        height =self.temp_img.shape[0]
+        width =self.temp_img.shape[1]
+
+        rectangle = []
+        for loc in locations:
+            rect = [int(loc[0]),int(loc[1]),width,height]
+            rectangle.append(rect)
+            rectangle.append(rect)
+        
+
+        point = []
+        rex,weights = cv.groupRectangles(rectangle,groupThreshold=1,eps=0.5)
+        
+        exit
+        
+        #เข้ามาแก้ในนี้
+       
+        if len(rex):
+            for (x,y,w,h) in rex:
+               
+                topleft = (x,y)
+                bottomright = ( x+w , y+h )
+                
+                centerX = x + int(w /2 )
+                centerY = y + int(h /2)
+                point.append((centerX,centerY))
+                
+                if debug:
+                    cv.rectangle(self.mainimg,topleft,bottomright,color=(255,0,255),thickness=2 , lineType=cv.LINE_8) 
+                    cv.drawMarker(self.mainimg,(centerX,centerY),color=(0,222,255),thickness=2,markerSize=50,markerType=cv.MARKER_DIAMOND)
+        else:
+            print('ไม่เจอรูปภาพ')  
+            
+        if debug:
+            print(f'เจอรูปภาพทั้งหมด = {len(rex)}')    
+            print(point)      
+            cv.imshow('result',self.mainimg)
+            cv.waitKey()
+            cv.destroyAllWindows()
+        return point
+            
+# แก้ตรงนี้
+mybot = Classbot('image/Screenshot3test.jpg','image/box.jpg')    
+mypoint = mybot.search(debug=True)  
